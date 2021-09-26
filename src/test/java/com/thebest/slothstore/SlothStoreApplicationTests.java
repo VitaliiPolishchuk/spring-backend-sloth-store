1de0a383836351e7b5ccbd76253ea3420301ab81
Vitalii <vitalii0polishchuk@gmail.com>
2021-09-26 23:23:03 +0300

    First commit

---
+++ src/test/java/com/thebest/slothstore/SlothStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thebest.slothstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlothStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
